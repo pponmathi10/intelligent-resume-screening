9f1b60e94b09885dc677e22cf257e2ae275e96a5
pponmathi10 <pponmathi10@gmail.com>
2026-01-10 19:28:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import PyPDF2
