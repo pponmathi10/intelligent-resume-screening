ddbf1d218d4516ca626594ba0808f3bdfde8e3fa
pponmathi10 <pponmathi10@gmail.com>
2026-01-10 19:29:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import PyPDF2
 import io
